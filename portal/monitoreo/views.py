d43fc214e1b73152da580410b4188397c488284d
julscode <jcah549@gmail.com>
2026-08-18 23:24:17 -0600

    agregando plantilla basica de monitoreo

--- portal/monitoreo/views.py
+++ portal/monitoreo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def monitoreo(request):
+    return render(request,'monitoreo/monitoreo.html')
+
+def detalles(request):
+    return render(request,'monitoreo/detalles.html')
